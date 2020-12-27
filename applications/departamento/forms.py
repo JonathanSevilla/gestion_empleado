862191dc1af693a380612969c6da973323b43feb
Jonathan Sevilla <jonathansevilla.7@gmail.com>
2020-12-27 12:49:01 -0600

    Add files via upload

---
+++ applications/departamento/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class new_departamento(forms.Form):
+    nombre = forms.CharField(max_length=50)
+    apellido = forms.CharField(max_length=50)
+    departamento = forms.CharField(max_length=50)
+    shortname = forms.CharField(max_length=50)
+    
